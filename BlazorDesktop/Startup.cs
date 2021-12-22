b378d2cf33448326cdf402e6d6b2cadce1361c58
周 <zqyo2000z@hotmail.com>
2021-12-22 15:26:41 +0800

    添加项目文件。

---
+++ BlazorDesktop/Startup.cs
@@ -0,0 +1,77 @@
+using BlazorDesktop.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+
+namespace BlazorDesktop
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            // if (HybridSupport.IsElectronActive)
+            // {
+            //    var window= Electron.WindowManager.
+            // }
+
+            Task.Run(async () =>
+            {
+                var window = await Electron.WindowManager.CreateWindowAsync(
+                    new BrowserWindowOptions
+                    {
+                        HasShadow = true,
+                        AutoHideMenuBar = true, //×Ô¶¯Òþ²Ø²Ëµ¥À¸
+                         Fullscreenable = true
+                    });
+                window.SetTitle("Blazor Demo App");
+            });
+
+
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
